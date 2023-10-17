16ae870a19ce7d8be24d85190a80261393a86b0c
miox <gallantsummerwind@gmail.com>
2023-10-17 23:22:30 +0900

    Routeの記載を修正して、ルーティングができるように修正

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import Home from './components/Home'
-import Gaming from './components/Gaming'
-import Learnign from './components/Learnign'
-import Live from './components/Live'
-import Movies from './components/Movies'
-import Music from './components/Music'
-import News from './components/News'
-import Trending from './components/Trending'
-import Sports from './components/Sports'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Gaming from "./components/Gaming";
+import Learning from "./components/Learning";
+import Live from "./components/Live";
+import Movies from "./components/Movies";
+import Music from "./components/Music";
+import News from "./components/News";
+import Trending from "./components/Trending";
+import Sports from "./components/Sports";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/gaming" element={<Gaming />} />
+          <Route path="/learning" element={<Learning />} />
+          <Route path="/live" element={<Live />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/sports" element={<Sports />} />
+        </Routes>
+    </>
   );
 }
 
--- src/components/Learnign.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Learnign = () => {
-  return (
-    <div>Learnign</div>
-  )
-}
-
-export default Learnign
